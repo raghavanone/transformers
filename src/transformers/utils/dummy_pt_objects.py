a813aa9dff446dae7cd107c5cbfebff149986607
Niels <niels.rogge1@gmail.com>
2023-11-04 20:32:46 +0100

    Rename Beit3ForVisualReasoning

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -1093,6 +1093,13 @@ class Beit3ForImageClassification(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+class Beit3ForImagesAndTextClassification(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 class Beit3ForImageTextRetrieval(metaclass=DummyObject):
     _backends = ["torch"]
 
@@ -1101,13 +1108,6 @@ class Beit3ForImageTextRetrieval(metaclass=DummyObject):
 
 
 class Beit3ForQuestionAnswering(metaclass=DummyObject):
-    _backends = ["torch"]
-
-    def __init__(self, *args, **kwargs):
-        requires_backends(self, ["torch"])
-
-
-class Beit3ForVisualReasoning(metaclass=DummyObject):
     _backends = ["torch"]
 
     def __init__(self, *args, **kwargs):
